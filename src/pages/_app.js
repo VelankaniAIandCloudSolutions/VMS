ba70f65af91987b2f1c3125215683a83a036b811
satvik <katochsatvik@gmail.com>
2024-06-21 17:31:46 +0530

    fixed black flicker theme prob, on page refresh

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,13 +3,20 @@ import { AppCacheProvider } from "@mui/material-nextjs/v13-pagesRouter";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
-
+import { useState } from "react";
 import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [theme, setTheme] = useState(null);
+
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    let theme = localStorage.getItem("theme") || "light";
+    setTheme(theme);
   }, []);
+  if (!theme) {
+    return; // `theme` is null in the first render
+  }
   return (
     <AppCacheProvider {...pageProps}>
       <Component {...pageProps} />
