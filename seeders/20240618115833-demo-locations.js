46c281ffd8ad544609ff93e70b4bf23bd9abe7db
Ankit Raj <ankitrajgaya1996@gmail.com>
2024-06-27 09:55:23 +0530

    Seeders

--- seeders/20240618115833-demo-locations.js
+++ seeders/20240618115833-demo-locations.js
@@ -1,25 +1,26 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    const locationsData = [
+      {
+        location_name: "Office A",
+        address: "123 Main St, City A, State A",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        location_name: "Office B",
+        address: "456 Elm St, City B, State B",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ];
+    await queryInterface.bulkInsert("Locations", locationsData, {});
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Locations", null, {});
+  },
 };
--- seeders/20240618115841-demo-visits.js
+++ seeders/20240618115841-demo-visits.js
@@ -3,23 +3,37 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-     */
+    const visitsData = [
+      {
+        visit_id: 1,
+        visitor_id: 1,
+        host_id: 1,
+        checkin_time: new Date(),
+        checkout_time: new Date(),
+        status: "completed",
+        visit_type_id: 1,
+        location_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        visit_id: 2,
+        visitor_id: 2,
+        host_id: 1,
+        checkin_time: new Date(),
+        checkout_time: new Date(),
+        status: "completed",
+        visit_type_id: 3,
+        location_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ];
+
+    await queryInterface.bulkInsert("visits", visitsData, {});
   },
 
   async down(queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete("visits", null, {});
   },
 };
