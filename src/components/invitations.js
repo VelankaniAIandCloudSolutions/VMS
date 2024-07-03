622f71991267fc8378631cab73080f5f1e32ca12
Ankit Raj <ankitrajgaya1996@gmail.com>
2024-07-03 10:43:02 +0530

    smallChanges

--- src/components/invitations.js
+++ src/components/invitations.js
@@ -1,8 +1,12 @@
 import * as React from "react";
+import { useState } from "react";
+
 import { DataGrid, GridToolbarContainer } from "@mui/x-data-grid";
 import { Box, Button, MenuItem, TextField } from "@mui/material";
 import "react-toastify/dist/ReactToastify.css";
 import CircularProgress from "@mui/material/CircularProgress";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 function CustomToolbar({ filterStatus, setFilterStatus }) {
   return (
@@ -25,7 +29,235 @@ function CustomToolbar({ filterStatus, setFilterStatus }) {
     </GridToolbarContainer>
   );
 }
-const VisitsDataGrid = ({ visits, columns, filterStatus, setFilterStatus }) => {
+const VisitsDataGrid = ({
+  initialVisits,
+
+  sessionString,
+}) => {
+  const parsedSession = JSON.parse(sessionString);
+
+  const filteredVisits = initialVisits.filter(
+    (row) => row?.host_id === parsedSession?.user?.user_id
+  );
+
+  const [visits, setVisits] = useState(filteredVisits);
+  const [filterStatus, setFilterStatus] = React.useState("All");
+
+  console.log("parsedSession", parsedSession);
+  console.log("the stringSessionId:", parsedSession.user.user_id);
+  const isAdmin = parsedSession?.user?.role === "admin";
+  console.log("isAdmin:", isAdmin);
+
+  const handleApprove = (visitId) => {
+    axios
+      .put(`http://localhost:3000/api/invitations/${visitId}`, {
+        status: "Approved", // Set status to 'Approved' in the request body
+      })
+      .then((response) => {
+        if (response.status >= 200 && response.status < 300) {
+          // Update local state to reflect the approved visit
+          // const updatedVisits = visits.map((visit) =>
+          //   visit.id === visitId ? { ...visit, status: "Approved" } : visit
+          // );
+          setVisits(response.data.visits);
+          toast.success("Visit approved successfully!", {
+            position: "bottom-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+
+          // // Wait a brief moment before showing email notification
+          // setTimeout(() => {
+          //   // Notify about email sent
+          //   toast.info("The visitor has been notified of the status update.", {
+          //     position: "bottom-right",
+          //     autoClose: 3000,
+          //     hideProgressBar: false,
+          //     closeOnClick: true,
+          //     pauseOnHover: true,
+          //     draggable: true,
+          //     progress: undefined,
+          //     theme: "light",
+          //   });
+          // }, 4000); // Adjust timing as needed
+          // console.log(`Visit ${visitId} approved successfully`);
+          // Optionally update the UI or fetch data again
+        } else {
+          throw new Error(
+            `Failed to approve visit ${visitId}. Status: ${response.status}`
+          );
+        }
+      })
+      .catch((error) => {
+        // Handle error
+        console.error(`Error approving visit ${visitId}:`, error);
+        toast.error(`Error approving visit as ${error.response.data.error}`, {
+          position: "bottom-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      });
+  };
+
+  const handleReject = (visitId) => {
+    axios
+      .put(`http://localhost:3000/api/invitations/${visitId}`, {
+        status: "Declined", // Set status to 'Declined' in the request body
+      })
+      .then((response) => {
+        if (response.status >= 200 && response.status < 300) {
+          // Update local state to reflect the rejected visit
+          // const updatedVisits = visits.map((visit) =>
+          //   visit.id === visitId ? { ...visit, status: "Declined" } : visit
+          // );
+          setVisits(response.data.visits);
+          toast.success("Visit declined successfully!", {
+            position: "bottom-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+          // setTimeout(() => {
+          //   // Notify about email sent
+          //   toast.info("The visitor has been notified of the status update.", {
+          //     position: "bottom-right",
+          //     autoClose: 3000,
+          //     hideProgressBar: false,
+          //     closeOnClick: true,
+          //     pauseOnHover: true,
+          //     draggable: true,
+          //     progress: undefined,
+          //     theme: "light",
+          //   });
+          // }, 4000); // Adjust timing as needed
+
+          console.log(`Visit ${visitId} rejected successfully`);
+          // Optionally update the UI or fetch data again
+        } else {
+          throw new Error(
+            `Failed to reject visit ${visitId}. Status: ${response.status}`
+          );
+        }
+      })
+      .catch((error) => {
+        // Handle error
+        console.error(`Error rejecting visit ${visitId}:`, error);
+        toast.error(`Error declining visit as ${error.response.data.error}`, {
+          position: "bottom-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      });
+  };
+
+  const columns = [
+    { field: "visit_id", headerName: "ID", width: 90 },
+
+    {
+      field: "Visitor",
+      headerName: "Visitor Name",
+      width: 200,
+
+      valueGetter: (params) => {
+        // console.log("params in Visitor:", params); // Check what params contains
+        return `${params?.first_name || ""} ${params?.last_name || ""}`;
+      },
+    },
+    { field: "visit_date", headerName: "Visit Date", width: 150 },
+    { field: "visit_time", headerName: "Visit Time", width: 150 },
+    {
+      field: "Host",
+      headerName: "Host Name",
+      width: 200,
+
+      valueGetter: (params) => {
+        // console.log("params in Host:", params); // Check what params contains
+        return `${params?.first_name || ""} ${params?.last_name || ""}`;
+      },
+    },
+    { field: "host_id", headerName: "Host ID", width: 150 },
+    { field: "purpose", headerName: "Purpose", width: 200 },
+
+    {
+      field: "Location",
+      headerName: "Location",
+      width: 200,
+      valueGetter: (params) => {
+        return params?.location_name || "";
+      },
+    },
+
+    {
+      field: "VisitType",
+      headerName: "Visit Type",
+      width: 200,
+      valueGetter: (params) => {
+        return params?.visit_type || "";
+      },
+    },
+
+    { field: "status", headerName: "Status", width: 150 },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 200,
+      renderCell: (params) => {
+        const { row } = params;
+        console.log("isAdmin", isAdmin);
+        // console.log("session?.user?.user_id ", parsedSession  );
+        const isHost = row?.host_id === parsedSession?.user?.user_id;
+        console.log("row?.host_id ", row?.host_id);
+        console.log("isHost ", isHost);
+
+        return (
+          <>
+            <Button
+              variant="contained"
+              color="success"
+              size="small"
+              sx={{ mr: 1 }}
+              onClick={() => handleApprove(params.row.visit_id)}
+              disabled={params?.row?.status === "Approved"}
+              // disabled={isAdmin ? false : !isHost}
+            >
+              Approve
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              size="small"
+              onClick={() => {
+                handleReject(params.row.visit_id);
+              }}
+              disabled={params?.row?.status === "Declined"}
+              // disabled={isAdmin ? false : !isHost}
+            >
+              Reject
+            </Button>
+          </>
+        );
+      },
+    },
+  ];
   return (
     <Box
       sx={{
--- src/pages/logbook.js
+++ src/pages/logbook.js
@@ -29,6 +29,7 @@ import Modal from "@/components/Modal";
 import ScheduleVisitForm from "@/components/ScheduleVisitForm";
 import { toast } from "react-toastify";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import FilteredVisitsDataGrid from "@/components/logbook";
 
 const theme = createTheme();
 
@@ -82,6 +83,7 @@ const Logbook = ({ visit, users, locations }) => {
   const [updatedVisit, setUpdatedVisit] = useState(visit);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [filter, setFilter] = useState("All");
+  const [searchQuery, setSearchQuery] = useState("");
 
   console.log("visits", visit);
   console.log("locations", users);
@@ -110,21 +112,33 @@ const Logbook = ({ visit, users, locations }) => {
     );
   };
 
+  const handleUpdatedVisits = (updatedVisits) => {
+    setUpdatedVisit(updatedVisits);
+  };
+
   const filteredVisits = updatedVisit.filter((row) => {
+    const visitorName =
+      `${row.Visitor.first_name} ${row.Visitor.last_name}`.toLowerCase();
+    const nameMatch = visitorName.includes(searchQuery.toLowerCase());
+
     const visitDate = new Date(row.visit_date_time);
     const selectedDate = date ? new Date(date) : null;
 
+    let dateMatch = true;
     if (selectedDate && !isSameDay(visitDate, selectedDate)) {
-      return false;
+      dateMatch = false;
     }
+
+    let statusMatch = true;
     if (filter === "Expected") {
-      return !row.checkin_time;
+      statusMatch = !row.checkin_time;
     } else if (filter === "Checked in") {
-      return row.checkin_time && !row.checkout_time;
+      statusMatch = row.checkin_time && !row.checkout_time;
     } else if (filter === "Checked out") {
-      return row.checkin_time && row.checkout_time;
+      statusMatch = row.checkin_time && row.checkout_time;
     }
-    return true;
+
+    return nameMatch && dateMatch && statusMatch;
   });
 
   const CustomListItem = styled(ListItem)({
@@ -136,174 +150,10 @@ const Logbook = ({ visit, users, locations }) => {
     margin: 0,
   });
 
-  const formatTime12hr = (dateString) => {
-    const date = new Date(dateString);
-    return date.toLocaleTimeString("en-US", {
-      hour: "numeric",
-      minute: "numeric",
-      hour12: true,
-    });
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
   };
 
-  const formatDateToMySQL = (datetime) => {
-    const pad = (n) => (n < 10 ? "0" + n : n);
-
-    const year = datetime.getFullYear();
-    const month = pad(datetime.getMonth() + 1);
-    const day = pad(datetime.getDate());
-    const hours = pad(datetime.getHours());
-    const minutes = pad(datetime.getMinutes());
-    const seconds = pad(datetime.getSeconds());
-
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-  };
-
-  const handleCheckIn = async (visitId) => {
-    try {
-      const currentDateTime = new Date();
-      const formattedCheckInTime = formatDateToMySQL(currentDateTime);
-      const response = await axios.put(
-        `http://localhost:3000/api/logbook/${visitId}`,
-        {
-          checkin_time: formattedCheckInTime,
-        }
-      );
-      setUpdatedVisit(response.data.visits);
-      toast.success("CheckIn Successfully. ", {
-        position: "bottom-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-    } catch (error) {
-      toast.error("Error in CheckIn . ", {
-        position: "bottom-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-
-      console.error("Error updating check-in:", error);
-    }
-  };
-
-  const handleCheckOut = async (visitId) => {
-    try {
-      const currentDateTime = new Date();
-      const formattedCheckInTime = formatDateToMySQL(currentDateTime);
-      const response = await axios.put(
-        `http://localhost:3000/api/logbook/${visitId}`,
-        {
-          checkout_time: formattedCheckInTime,
-        }
-      );
-      setUpdatedVisit(response.data.visits);
-      toast.success("Checkout Successfully. ", {
-        position: "bottom-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-    } catch (error) {
-      toast.error("Error in CheckOut . ", {
-        position: "bottom-right",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
-      console.error("Error updating check-out:", error);
-    }
-  };
-
-  const calculateDurationInMinutes = (checkinTime, checkoutTime) => {
-    if (checkinTime && checkoutTime) {
-      const duration =
-        (new Date(checkoutTime) - new Date(checkinTime)) / (1000 * 60); // convert to minutes
-      return duration.toFixed(2) + " minutes";
-    }
-    return "N/A";
-  };
-
-  const columns = [
-    {
-      field: "Visitor",
-      headerName: "Visitor",
-      width: 200,
-      valueGetter: (params) => `${params.first_name} ${params.last_name}`,
-    },
-    {
-      field: "Host",
-      headerName: "Host",
-      width: 200,
-      valueGetter: (params) => `${params.first_name} ${params.last_name}`,
-    },
-    { field: "confirmation_id", headerName: "ID", width: 120, sortable: false },
-    {
-      field: "checkin_time",
-      headerName: "Check in",
-      width: 160,
-      renderCell: (params) => {
-        if (params.row.checkin_time) {
-          const checkInTime = formatTime12hr(params.row.checkin_time);
-          return checkInTime;
-        } else {
-          return (
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => handleCheckIn(params.row.visit_id)}
-            >
-              Check in
-            </Button>
-          );
-        }
-      },
-    },
-    {
-      field: "checkout_time",
-      headerName: "Check out",
-      width: 160,
-      renderCell: (params) => {
-        if (params.row.checkout_time) {
-          const checkOutTime = formatTime12hr(params.row.checkout_time);
-          return checkOutTime;
-        } else {
-          return (
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={() => handleCheckOut(params.row.visit_id)}
-              disabled={!params.row.checkin_time}
-            >
-              Check Out
-            </Button>
-          );
-        }
-      },
-    },
-    {
-      field: "duration",
-      headerName: "Duration",
-      width: 120,
-      renderCell: (params) => {
-        const checkinTime = params.row.checkin_time;
-        const checkoutTime = params.row.checkout_time;
-        return calculateDurationInMinutes(checkinTime, checkoutTime);
-      },
-    },
-  ];
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
@@ -380,6 +230,8 @@ const Logbook = ({ visit, users, locations }) => {
                   label="Search"
                   variant="outlined"
                   sx={{ width: "100%" }}
+                  value={searchQuery}
+                  onChange={handleSearch}
                 />
               </Grid>
             </Grid>
@@ -440,12 +292,10 @@ const Logbook = ({ visit, users, locations }) => {
               </Grid>
 
               <Grid item xs={9}>
-                <DataGrid
-                  className="custom-datagrid"
-                  rows={filteredVisits}
-                  columns={columns}
-                  getRowId={(row) => row.visit_id}
-                  pageSize={5}
+                <FilteredVisitsDataGrid
+                  filteredVisits={filteredVisits}
+                  visit={visit}
+                  onUpdatedVisits={handleUpdatedVisits}
                 />
               </Grid>
             </Grid>
