4dfd4885e11f06136c53325f1e96c7da4891ed64
satvik <katochsatvik@gmail.com>
2024-07-23 15:06:58 +0530

    changed back to secrets

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,16 +1,8 @@
-// // utils/axiosConfig.js
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-// });
-
-// export default axiosInstance;
-
+// utils/axiosConfig.js
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://vms.automhr.com",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
 export default axiosInstance;
--- src/utils/sequelize.js
+++ src/utils/sequelize.js
@@ -15,63 +15,35 @@
 
 // module.exports = sequelize;
 
-// const { Sequelize } = require("sequelize");
-// require("dotenv").config(); // Load environment variables from .env file
-
-// // Determine environment (default to 'development')
-// const env = process.env.NODE_ENV || "development";
-
-// // Load configuration from config.json based on environment
-// const config = require("../../config/config.json")[env];
-// // const config = require("../../config/config.json");
-
-// // Use environment variables if available, otherwise fall back to config.json values
-// const sequelize = new Sequelize(
-//   process.env.DB_DATABASE || config.database,
-//   process.env.DB_USERNAME || config.username,
-//   process.env.DB_PASSWORD || config.password,
-//   {
-//     host: process.env.DB_HOST || config.host,
-//     dialect: "mysql",
-//     dialectModule: require("mysql2"),
-//     logging: false, // Disable logging to avoid clutter
-//   }
-// );
-
-// // Test the connection
-// async function testConnection() {
-//   try {
-//     await sequelize.authenticate();
-//     console.log("Database connection to  has been established successfully.");
-//   } catch (error) {
-//     console.error("Unable to connect to the database:", error);
-//   }
-// }
-
-// testConnection();
-
-// module.exports = sequelize;
-
 const { Sequelize } = require("sequelize");
+require("dotenv").config(); // Load environment variables from .env file
 
-// Hardcoded database credentials
+// Determine environment (default to 'development')
+const env = process.env.NODE_ENV || "development";
+
+// Load configuration from config.json based on environment
+const config = require("../../config/config.json")[env];
+// const config = require("../../config/config.json");
+
+// Use environment variables if available, otherwise fall back to config.json values
 const sequelize = new Sequelize(
-  "visitor_management", // database name
-  "admin", // username
-  "Password@123", // password
+  process.env.DB_DATABASE || config.database,
+  process.env.DB_USERNAME || config.username,
+  process.env.DB_PASSWORD || config.password,
   {
-    host: "13.233.163.245", // database host
+    host: process.env.DB_HOST || config.host,
     dialect: "mysql",
     dialectModule: require("mysql2"),
     logging: false, // Disable logging to avoid clutter
   }
 );
+//
 
 // Test the connection
 async function testConnection() {
   try {
     await sequelize.authenticate();
-    console.log("Database connection has been established successfully.");
+    console.log("Database connection to  has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
@@ -80,3 +52,4 @@ async function testConnection() {
 testConnection();
 
 module.exports = sequelize;
+//
