100cce9263503a6930b04a4552c7f9bc135287a7
satvik <katochsatvik@gmail.com>
2024-07-24 15:05:37 +0530

    changes

--- src/pages/test.js
+++ src/pages/test.js
@@ -22,15 +22,39 @@ import useSWR from "swr";
 import React from "react";
 
 // Define a fetcher function using Axios
+
 const fetcher = (url) => axiosInstance.get(url).then((res) => res.data);
 
 export default function Home() {
   // Use SWR to fetch data
-  const { data, error } = useSWR("/api/test", fetcher);
+  const { data, error } = useSWR("/api/get-data", fetcher);
 
   // Display the data or handle loading/error states
   if (error) return <div>Error fetching data</div>;
   if (!data) return <div>Loading...</div>;
 
-  return <div>{data.message}</div>;
+  const { visitTypes, users, locations } = data;
+
+  return (
+    <div>
+      <h2>Visit Types</h2>
+      <ul>
+        {visitTypes.map((type) => (
+          <li key={type.id}>{type.type_name}</li>
+        ))}
+      </ul>
+      <h2>Users</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.first_name}</li>
+        ))}
+      </ul>
+      <h2>Locations</h2>
+      <ul>
+        {locations.map((location) => (
+          <li key={location.id}>{location.location_name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
