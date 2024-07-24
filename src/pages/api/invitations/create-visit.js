a90bc37319be754e6c4482ab33ecd8e844fdb503
Ankit Raj <ankitrajgaya1996@gmail.com>
2024-07-24 15:00:36 +0530

    apiChanges

--- src/pages/api/invitations/create-visit.js
+++ src/pages/api/invitations/create-visit.js
@@ -143,114 +143,20 @@ export default async function handler(req, res) {
         const users = await User.findAll();
         const locations = await Location.findAll();
 
-        res.status(200).json({ visitTypes, users, locations });
+        const jsonResponse = { visitTypes, users, locations };
+        console.log("Response of create visit-backend:", jsonResponse);
+
+        res.status(200).json(jsonResponse);
       } catch (error) {
-        console.error("Error fetching data:", error);
-        res
-          .status(500)
-          .json({ error: error.message || "Failed to fetch data" });
-      }
-    } else if (req.method === "POST") {
-      try {
-        const session = await getServerSession(req, res, authOptions);
-        const {
-          visit_date_time,
-          email,
-          firstName,
-          lastName,
-          host_id,
-          location_id,
-          phone,
-          purpose,
-          visit_type_id,
-        } = req.body;
-
-        let visitor = await User.findOne({ where: { email } });
-
-        if (!visitor) {
-          const userRole = await Role.findOne({ where: { role_name: "user" } });
-          if (!userRole) {
-            console.error("Default role 'user' not found in the database.");
-            return res
-              .status(500)
-              .json({ error: "Failed to assign role to user" });
-          }
-
-          const defaultPassword = "password";
-          const hashedPassword = await bcrypt.hash(defaultPassword, 10);
-
-          visitor = await User.create({
-            email,
-            first_name: firstName,
-            last_name: lastName,
-            phone_number: phone,
-            password: hashedPassword,
-            role_id: userRole.role_id,
-          });
-        }
-
-        const userStatus = session ? "Approved" : "Pending";
-        const newVisit = await Visit.create({
-          visit_date_time,
-          visitor_id: visitor.user_id,
-          host_id,
-          location_id,
-          phone,
-          purpose,
-          visit_type_id,
-          status: userStatus,
-        });
-
-        const fullVisit = await Visit.findByPk(newVisit.visit_id, {
-          include: [
-            { model: User, as: "Visitor" },
-            { model: User, as: "Host" },
-            { model: VisitType },
-            { model: Location },
-          ],
-        });
-
-        if (!fullVisit) {
-          throw new Error("Failed to fetch full visit details");
-        }
-
-        const visitDetails = {
-          ...fullVisit.toJSON(),
-          visitor: fullVisit.Visitor.toJSON(),
-          host: fullVisit.Host.toJSON(),
-          visit_type: fullVisit.VisitType.toJSON(),
-          location: fullVisit.Location.toJSON(),
-          visitDateFormatted: moment(fullVisit.visit_date_time).format(
-            "DD-MM-YYYY"
-          ),
-          visitTimeFormatted: moment(fullVisit.visit_date_time).format(
-            "hh:mm:ss A"
-          ),
-        };
-
-        res
-          .status(201)
-          .json({ message: "Visit created successfully", visit: fullVisit });
-
-        try {
-          await sendEmail(visitDetails);
-          console.log("Confirmation email sent successfully");
-        } catch (emailError) {
-          console.error("Error sending confirmation email:", emailError);
-          // Handle email sending error
-        }
-      } catch (error) {
-        console.error("Error creating or processing visit:", error);
-        res
-          .status(500)
-          .json({ error: error.message || "Failed to create visit" });
+        console.error("Error fetching data:", error.message);
+        res.status(500).json({ error: "Failed to fetch data" });
       }
     } else {
-      res.setHeader("Allow", ["GET", "POST"]);
+      res.setHeader("Allow", ["GET"]);
       res.status(405).end(`Method ${req.method} Not Allowed`);
     }
   } catch (error) {
-    console.error("Unexpected error processing request:", error);
-    res.status(500).json({ error: error.message || "Unexpected server error" });
+    console.error("Error processing request:", error.message);
+    res.status(500).json({ error: "Failed to process request" });
   }
 }
