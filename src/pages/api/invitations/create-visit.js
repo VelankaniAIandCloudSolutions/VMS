eab0b16fd6817dd8537d965c18ebd8edeac7fb62
Ankit Raj <ankitrajgaya1996@gmail.com>
2024-07-24 15:18:45 +0530

    dfg

--- src/pages/api/invitations/create-visit.js
+++ src/pages/api/invitations/create-visit.js
@@ -1,3 +1,5 @@
+import moment from "moment-timezone";
+moment.tz.setDefault("Asia/Kolkata");
 const VisitType = require("../../../../models/VisitTypes");
 const User = require("../../../../models/Users");
 const Location = require("../../../../models/Locations");
@@ -5,124 +7,112 @@ const Visit = require("../../../../models/Visits");
 const Role = require("../../../../models/Roles");
 const bcrypt = require("bcryptjs");
 const { sendEmail } = require("../../../utils/email");
-import moment from "moment-timezone";
-moment.tz.setDefault("Asia/Kolkata");
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(req, res) {
-  // Set CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST"); // Allow specific methods
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type"); // Allow specific headers
-
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-
   try {
-    if (req.method === "GET") {
-      const visitTypes = await VisitType.findAll();
-      const users = await User.findAll();
-      const locations = await Location.findAll();
-
-      res.status(200).json({ visitTypes, users, locations });
-    } else if (req.method === "POST") {
-      const session = await getServerSession(req, res, authOptions);
-
-      const {
-        visit_date_time,
-        email,
-        firstName,
-        lastName,
-        host_id,
-        location_id,
-        phone,
-        purpose,
-        visit_type_id,
-      } = req.body;
-
-      let visitor = await User.findOne({ where: { email } });
-
-      if (!visitor) {
-        const userRole = await Role.findOne({ where: { role_name: "user" } });
-        const defaultPassword = "password";
-        const hashedPassword = await bcrypt.hash(defaultPassword, 10);
-
-        visitor = await User.create({
+    if (req.method === "POST") {
+      try {
+        const session = await getServerSession(req, res, authOptions);
+        const {
+          visit_date_time,
           email,
-          first_name: firstName,
-          last_name: lastName,
-          phone_number: phone,
-          password: hashedPassword,
-          role_id: userRole ? userRole.role_id : null,
+          firstName,
+          lastName,
+          host_id,
+          location_id,
+          phone,
+          purpose,
+          visit_type_id,
+        } = req.body;
+
+        let visitor = await User.findOne({ where: { email } });
+
+        if (!visitor) {
+          const userRole = await Role.findOne({ where: { role_name: "user" } });
+          if (!userRole) {
+            console.error("Default role 'user' not found in the database.");
+            return res
+              .status(500)
+              .json({ error: "Failed to assign role to user" });
+          }
+
+          const defaultPassword = "password";
+          const hashedPassword = await bcrypt.hash(defaultPassword, 10);
+
+          visitor = await User.create({
+            email,
+            first_name: firstName,
+            last_name: lastName,
+            phone_number: phone,
+            password: hashedPassword,
+            role_id: userRole.role_id,
+          });
+        }
+
+        const userStatus = session ? "Approved" : "Pending";
+        const newVisit = await Visit.create({
+          visit_date_time,
+          visitor_id: visitor.user_id,
+          host_id,
+          location_id,
+          phone,
+          purpose,
+          visit_type_id,
+          status: userStatus,
         });
 
-        if (!userRole) {
-          console.error("Default role 'user' not found in the database.");
-          res.status(500).json({ error: "Failed to assign role to user" });
-          return;
+        const fullVisit = await Visit.findByPk(newVisit.visit_id, {
+          include: [
+            { model: User, as: "Visitor" },
+            { model: User, as: "Host" },
+            { model: VisitType },
+            { model: Location },
+          ],
+        });
+
+        if (!fullVisit) {
+          throw new Error("Failed to fetch full visit details");
         }
-      }
 
-      const userStatus = session ? "Approved" : "Pending";
-      const newVisit = await Visit.create({
-        visit_date_time,
-        visitor_id: visitor.user_id,
-        host_id,
-        location_id,
-        phone,
-        purpose,
-        visit_type_id,
-        status: userStatus,
-      });
+        const visitDetails = {
+          ...fullVisit.toJSON(),
+          visitor: fullVisit.Visitor.toJSON(),
+          host: fullVisit.Host.toJSON(),
+          visit_type: fullVisit.VisitType.toJSON(),
+          location: fullVisit.Location.toJSON(),
+          visitDateFormatted: moment(fullVisit.visit_date_time).format(
+            "DD-MM-YYYY"
+          ),
+          visitTimeFormatted: moment(fullVisit.visit_date_time).format(
+            "hh:mm:ss A"
+          ),
+        };
 
-      const fullVisit = await Visit.findByPk(newVisit.visit_id, {
-        include: [
-          { model: User, as: "Visitor" },
-          { model: User, as: "Host" },
-          { model: VisitType },
-          { model: Location },
-        ],
-      });
+        res
+          .status(201)
+          .json({ message: "Visit created successfully", visit: fullVisit });
 
-      if (!fullVisit) {
-        throw new Error("Failed to fetch full visit details");
-      }
-
-      const visitDetails = {
-        ...fullVisit.toJSON(),
-        visitor: fullVisit.Visitor.toJSON(),
-        host: fullVisit.Host.toJSON(),
-        visit_type: fullVisit.VisitType.toJSON(),
-        location: fullVisit.Location.toJSON(),
-        visitDateFormatted: moment(fullVisit.visit_date_time).format(
-          "DD-MM-YYYY"
-        ),
-        visitTimeFormatted: moment(fullVisit.visit_date_time).format(
-          "hh:mm:ss A"
-        ),
-      };
-
-      res
-        .status(201)
-        .json({ message: "Visit created successfully", visit: fullVisit });
-
-      try {
-        await sendEmail(visitDetails);
-        console.log("Confirmation email sent successfully");
+        try {
+          await sendEmail(visitDetails);
+          console.log("Confirmation email sent successfully");
+        } catch (emailError) {
+          console.error("Error sending confirmation email:", emailError);
+          // Handle email sending error
+        }
       } catch (error) {
-        console.error("Error sending confirmation email:", error.response.data);
-        // Handle email sending error
+        console.error("Error creating or processing visit:", error);
+        res
+          .status(500)
+          .json({ error: error.message || "Failed to create visit" });
       }
     } else {
-      res.setHeader("Allow", ["GET", "POST"]);
+      res.setHeader("Allow", ["POST"]);
       res.status(405).end(`Method ${req.method} Not Allowed`);
     }
   } catch (error) {
-    console.error("Error processing request:", error.message);
-    res.status(500).json({ error: "Failed to process request" });
+    console.error("Unexpected error processing request:", error);
+    res.status(500).json({ error: error.message || "Unexpected server error" });
   }
 }
