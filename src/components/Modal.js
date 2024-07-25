76a427748d054554ef5e0a34b6d59d2feb921faa
satvik <katochsatvik@gmail.com>
2024-07-25 11:20:45 +0530

    made modal resposnive

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,73 @@
 // components/BasicModal.js
 
+// import React from "react";
+// import Box from "@mui/material/Box";
+// import Typography from "@mui/material/Typography";
+// import Modal from "@mui/material/Modal";
+
+// const style = {
+//   position: "absolute",
+//   top: "50%",
+//   left: "50%",
+//   transform: "translate(-50%, -50%)",
+//   width: "auto",
+//   height: "auto", // Set width as a percentage for responsiveness
+//   maxWidth: 600, // Set max width in pixels
+//   bgcolor: "background.paper",
+//   boxShadow: 24,
+//   p: 4,
+// };
+
+// export default function BasicModal({
+//   open,
+//   handleClose,
+//   children,
+//   title,
+//   mode,
+// }) {
+//   const isEditMode = mode === "edit";
+//   const modalTitle = mode
+//     ? isEditMode
+//       ? `Edit ${title}`
+//       : `Add ${title}`
+//     : title;
+
+//   return (
+//     <Modal
+//       open={open}
+//       onClose={handleClose}
+//       aria-labelledby="modal-modal-title"
+//       aria-describedby="modal-modal-description"
+//     >
+//       <Box sx={style}>
+//         <Typography
+//           id="modal-modal-title"
+//           variant="h5"
+//           component="h2"
+//           sx={{
+//             mb: 2,
+//             fontWeight: "bold",
+//             textAlign: "center",
+//             color: "primary.main",
+//             position: "relative",
+//             "&::after": {
+//               content: '""',
+//               display: "block",
+//               width: "50%",
+//               height: "2px",
+//               backgroundColor: "primary.main",
+//               margin: "8px auto 0",
+//             },
+//           }}
+//         >
+//           {modalTitle}
+//         </Typography>
+//         <Box>{children}</Box>
+//       </Box>
+//     </Modal>
+//   );
+// }
+
 import React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
@@ -10,9 +78,9 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: "auto",
-  height: "auto", // Set width as a percentage for responsiveness
-  maxWidth: 600, // Set max width in pixels
+  width: "90%", // Responsive width
+  maxWidth: 600, // Maximum width
+  height: "auto",
   bgcolor: "background.paper",
   boxShadow: 24,
   p: 4,
@@ -39,7 +107,12 @@ export default function BasicModal({
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Box sx={style}>
+      <Box
+        sx={{
+          ...style,
+          width: { xs: "90%", sm: "80%", md: "60%" },
+        }}
+      >
         <Typography
           id="modal-modal-title"
           variant="h5"
@@ -50,6 +123,7 @@ export default function BasicModal({
             textAlign: "center",
             color: "primary.main",
             position: "relative",
+            fontSize: { xs: "h6.fontSize", sm: "h5.fontSize" },
             "&::after": {
               content: '""',
               display: "block",
@@ -62,7 +136,14 @@ export default function BasicModal({
         >
           {modalTitle}
         </Typography>
-        <Box>{children}</Box>
+        <Box
+          sx={{
+            overflowY: "auto",
+            maxHeight: "70vh",
+          }}
+        >
+          {children}
+        </Box>
       </Box>
     </Modal>
   );
