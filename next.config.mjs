81616deaf1bc9269e0df785571cea587581c2c9e
satvik <katochsatvik@gmail.com>
2024-07-30 14:04:44 +0530

    cors in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   async redirects() {
+//     return [
+//       {
+//         source: "/",
+//         destination: "/welcome",
+//         permanent: true,
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,6 +23,27 @@ const nextConfig = {
         source: "/",
         destination: "/welcome",
         permanent: true,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
       },
     ];
   },
